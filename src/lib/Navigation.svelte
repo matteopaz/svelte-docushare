a130d8c3ee41f2f5b851eead31f7ab2fd03663ea
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2021-10-30 01:26:56 -0700

    Changed font and stripe, added blank cards and changed header

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,45 +1,57 @@
 <script lang="ts">
-    export let loggedIn = false;
+	export let loggedIn = false;
 </script>
 
 <nav>
-    <a href="/" class="navitem">Home</a>
-    <a href="/" class="navitem">Create new document</a>
-    <a href="/" class="navitem">Edit existing document</a>
-    {#if loggedIn}
-      <a href="/" class="navitem">Library</a>
-      <a href="/" class="navitem">Log out</a>
-    {:else}
-    <a href="/" class="navitem">Log in</a>
-    <a href="/" class="navitem">Sign up</a>
-    {/if}
+	<a href="/" class="navitem">Home</a>
+	<a href="/" class="navitem">Create new document</a>
+	{#if loggedIn}
+		<a href="/" class="navitem">Library</a>
+		<a href="/" class="navitem">Log out</a>
+	{:else}
+		<a href="/" class="navitem">Log in</a>
+		<a href="/" class="navitem">Sign up</a>
+	{/if}
 </nav>
 
 <style lang="scss">
- nav {
-    height: 5.5rem;
-    border-bottom: 0.125rem solid var(--dark);
-    background-color: var(--offwhite);
-    display: flex;
-    align-items: center;
-    justify-content: center;
- }
+	nav {
+		height: 5rem;
+		border-bottom: 0.125rem solid #67676732;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
 
- .navitem {
-     margin: 0 max(5vw, 5rem);
-     color: black;
-     font-size: clamp(1.15vw, 1.25rem, 2vw);
-     letter-spacing: 0.15rem;
-     text-decoration: none;
-     font-family: 'Source Sans Pro', sans-serif;
-     border-color: transparent;
-     font-size: clamp(0.75rem, 1.33vw, 1.5rem);
-     text-shadow: 0 0.2rem var(--offwhite);
-     &:hover {
-        transition: border-color 0.4s ease;
-         border-bottom: 0.2rem solid var(--dark);
-         border-radius: 0.05rem;
-         margin-bottom: -0.2rem;
-     }
- }
+	.navitem {
+		margin: 0 max(5vw, 5rem);
+		color: black;
+		letter-spacing: 0.15rem;
+		text-decoration: none;
+		font-family: 'Inter';
+		border-color: transparent;
+		font-size: clamp(1.15rem, 1.15vw, 1.5rem);
+        position: relative;
+		&::before {
+            opacity: 0.05;
+            transform: scale(0.001);
+			content: '';
+            position: absolute;
+            transition: all 0.2s ease-in-out;
+            z-index: -1;
+            opacity: 0.75;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            transform-origin: center;
+		}
+		&:hover::before {
+            opacity: 0.75;
+            transition: all 0.1s ease-in-out;
+			background-color: var(--offwhite);
+            border-radius: 0.35rem;
+            transform: scale(1.2);
+		}
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,32 +10,43 @@
 		<h1 class="hero">Svelte Docushare</h1>
 		<h2 class="hero">Document sharing and editing platform - A SvelteKit Demo</h2>
 		<div class="stripe" />
+		<div class="utilities-container">
+			<div class="card">
+			<ul class="statistics">
+				<li>📰 Documents Created Overall:&nbsp;&nbsp;<em>10</em></li>
+				<li>👀 Times Website Viewed:&nbsp;&nbsp;<em>10</em></li>
+
+			</ul>
+			</div>
+			<div class="card doclist" />
+		</div>
 	</section>
 </main>
 
 <style lang="scss">
 	:global(body) {
 		background-color: #e5e5e5;
+		font-family: 'Inter', sans-serif;
 	}
 	.stripe {
 		width: 35rem;
 		top: 0;
-    margin-left: 25vw;
-		height: 100rem;
+		margin-left: 25vw;
 		position: absolute;
-		background-color: black;
+		background-color: #be60b7;
 		transform: skew(-55deg);
 		z-index: -1;
+		height: 100vh;
+		opacity: 0.4;
+		background-image: linear-gradient(to bottom, var(--lin-grad-1));
+		background-repeat: no-repeat;
 	}
-
 	.hero-container {
 		position: relative;
 		padding: 5rem;
 		overflow: hidden;
-    min-height: calc(100vh - 5.5rem);
-
+		min-height: calc(100vh - 5rem);
 	}
-
 	.hero {
 		--txt: 100, 100, 100;
 		letter-spacing: 0.25rem;
@@ -50,16 +61,62 @@
 		float: left;
 		margin-top: 0;
 		margin-bottom: 0;
-		background: var(--lin-grad-1);
+		margin-right: 5rem;
+		background: linear-gradient(to top right, var(--lin-grad-1));
+		background-clip: text;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
 	h2.hero {
-		color: rgba(var(--txt), 0.75);
+		font-weight: normal;
+		color: black;
 		letter-spacing: 0.15rem;
 		margin: 0;
 		text-align: left;
 		padding-top: 2.5rem;
 		font-size: clamp(1.25rem, 2vw, 2.25rem);
+	}
+	.utilities-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: min(7vw, 8rem);
+		padding: 7.5rem 2.5rem 0 2.5rem;
+		height: 40rem;
+		clear: both;
+		place-items: center;
+		@media (max-width: 800px) {
+			grid-template-columns: unset;
+			grid-template-rows: 1fr 1fr;
+			height: 67.5rem;
+		}
+	}
+
+	.card {
+		background: #be60b744;
+		box-shadow: 0 8px 32px 0 rgba(128, 65, 123, 0.267);
+		backdrop-filter: blur(4px);
+		border-radius: 1rem;
+		border: 0.2rem solid #67676732;
+		padding: 5%;
+		height: 100%;
+		width: 100%;
+	}
+
+	.statistics {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		display: flex;
+		flex-flow: column wrap;
+		align-items: space-around;
+		gap: 5%;
+		justify-content: space-around;
+		li {
+			height: 5rem;
+		}
+		em {
+			font-size: 1.25em;
+		}
 	}
 </style>
