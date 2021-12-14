a342d64f9286d6a8de8fffad7d3d03656a84c94a
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2021-12-14 01:23:17 -0800

    Added Bearer to live token setting after login

--- src/lib/hooks/auth/handleLogin.ts
+++ src/lib/hooks/auth/handleLogin.ts
@@ -33,7 +33,7 @@ export default async function handleLogin(login: AuthenticationForm): Promise<bo
 		session.update(current => {
 			current.loggedIn = true;
 			current.user = parts[1].user;
-			current.jwt = token;
+			current.jwt = "Bearer " + token;
 			return current;
 		});
 	} else {
