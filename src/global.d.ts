071dd6b6367ce9227cafb41612b5f6cb91a27b66
matteopaz <86904497+matteopaz@users.noreply.github.com>
2022-02-25 12:09:05 -0800

    Env var conditionals

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,21 @@
 /// <reference types="@sveltejs/kit" />
-export const API_URL = `https://docushare.matteopaz.me/api`;
-export const DEPLOY_URL = `https://docushare.matteopaz.me`; 
+let api_url = "";
+let deploy_url = "";
+if (import.meta.env.PROD) {
+	api_url = `https://docushare.matteopaz.me/api`;
+	deploy_url = `https://docushare.matteopaz.me`;
+} else if (import.meta.env.STAGING) {
+	api_url = `https://docushare-lite.matteodev.workers.dev`;
+	deploy_url = `https://docushare-lite.matteodev.workers.dev`;
+} else {
+	api_url = `http://localhost:3000/api`;
+	deploy_url = `http://localhost:3000`;
+}
+export const API_URL = api_url;
+export const DEPLOY_URL = deploy_url;
 export interface AuthenticationForm {
-    email: string,
-    password: string,
-    status: string,
-    error: boolean
+	email: string;
+	password: string;
+	status: string;
+	error: boolean;
 }
